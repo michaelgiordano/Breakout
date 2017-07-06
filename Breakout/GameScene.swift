5013b4deb5659a9a55a67d71762f7f411f9f47b9
Michael Giordano <michaelgiordano@Michaels-MacBook-Air.local>
2017-07-06 11:13:47 -0500

    Created paddle dynamic behavior

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -29,6 +29,24 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         ball.physicsBody?.applyImpulse(CGVector(dx: 3, dy: 5))
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        for touch in touches
+        {
+            let location = touch.location(in: self)
+            paddle.position.x = location.x
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        for touch in touches
+        {
+            let location = touch.location(in: self)
+            paddle.position.x = location.x
+        }
+    }
+    
     func createBackground()
     {
         let stars = SKTexture(imageNamed: "stars")
